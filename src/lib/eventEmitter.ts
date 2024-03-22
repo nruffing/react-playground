b359f290589a209ae5114aef599a34fbda91b1cc
Nicholas Ruffing <nicholasruffing70@gmail.com>
2024-03-21 22:16:47 -0400

    event emitter ii

--- src/lib/eventEmitter.ts
+++ src/lib/eventEmitter.ts
@@ -9,7 +9,7 @@ interface IEventEmitter {
 // You are free to use alternative approaches of
 // instantiating the EventEmitter as long as the
 // default export is correct.
-export default class EventEmitter implements IEventEmitter {
+export class EventEmitter implements IEventEmitter {
   handlerMap = new Map<string, Function[]>()
 
   getHandlersForEvent(eventName: string) {
@@ -34,6 +34,50 @@ export default class EventEmitter implements IEventEmitter {
       handlers.splice(index, 1)
     }
     return this
+  }
+
+  emit(eventName: string, ...args: Array<any>): boolean {
+    const handlers = this.getHandlersForEvent(eventName)
+    for (const handler of handlers) {
+      handler(...args)
+    }
+    return !!handlers.length
+  }
+}
+
+
+// https://www.greatfrontend.com/questions/javascript/event-emitter-ii?list=data-structures-algorithms
+interface IEventEmitterII {
+  on(eventName: string, listener: Function): { off: () => void };
+  emit(eventName: string, ...args: Array<any>): boolean;
+}
+
+// You are free to use alternative approaches of
+// instantiating the EventEmitter as long as the
+// default export is correct.
+export default class EventEmitterII implements IEventEmitterII {
+  handlerMap = new Map<string, Function[]>()
+
+  getHandlersForEvent(eventName: string) {
+    let handlers = this.handlerMap.get(eventName)
+    if (!handlers) {
+      handlers = []
+      this.handlerMap.set(eventName, handlers)
+    }
+    return handlers
+  }
+
+  on(eventName: string, listener: Function): { off: () => void } {
+    const handlers = this.getHandlersForEvent(eventName)
+    handlers.push(listener)
+    return {
+      off: () => {
+        const index = handlers.findIndex(l => l === listener)
+        if (index >= 0) {
+          handlers.splice(index, 1)
+        }
+      }
+    }
   }
 
   emit(eventName: string, ...args: Array<any>): boolean {
